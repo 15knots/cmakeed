534273b787ebf64e3c0bb65eb95ffa6ac3804bc0
M. Jackson <imikejackson@users.sf.net>
2008-12-03 20:54:54 +0000

    Didn't get all the code committed the first time

--- 1_1_0_RC4/com.cthing.cmakeed.core/src/com/cthing/cmakeed/core/variables/package-info.java
+++
@@ -1,9 +0,0 @@
-/* *****************************************************************************
- * Copyright 2008 BlueQuartz Software, Michael Jackson
- * All Rights Reserved.
- ******************************************************************************/
-
-/**
- * Classes that provide information about CMake.
- */
-package com.cthing.cmakeed.core.variables;
--- 1_1_0_RC4/com.cthing.cmakeed.ui/src/com/cthing/cmakeed/ui/prefs/package-info.java
+++
@@ -1,9 +0,0 @@
-/* *****************************************************************************
- * Copyright 2007 C Thing Software
- * All Rights Reserved.
- ******************************************************************************/
-
-/**
- * Preference UI classes.
- */
-package com.cthing.cmakeed.ui.prefs;
