65285424868c073e7456f481214e5e2e669b65b6
M. Jackson <imikejackson@users.sf.net>
2008-12-03 20:55:34 +0000

    Tagging current source code with 1.1.0 RC4

--- com.cthing.cmakeed.core/src/com/cthing/cmakeed/core/package-info.java
+++
@@ -1,9 +0,0 @@
-/* *****************************************************************************
- * Copyright 2007 C Thing Software
- * All Rights Reserved.
- ******************************************************************************/
-
-/**
- * Top level classes for the CMakeEd core plugin.
- */
-package com.cthing.cmakeed.core;
