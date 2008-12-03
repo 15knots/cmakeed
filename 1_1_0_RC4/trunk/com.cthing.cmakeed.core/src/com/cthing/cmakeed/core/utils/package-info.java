afc6ee50c2c77267bf471e961e4b1c0bfabd2b0e
M. Jackson <imikejackson@users.sf.net>
2008-12-03 20:57:04 +0000

    Tagging current Source with 1.1.0 RC4

---
+++ 1_1_0_RC4/trunk/com.cthing.cmakeed.core/src/com/cthing/cmakeed/core/utils/package-info.java
@@ -0,0 +1,9 @@
+/* *****************************************************************************
+ * Copyright 2007 C Thing Software
+ * All Rights Reserved.
+ ******************************************************************************/
+
+/**
+ * Utility classes for the CMakeEd core plugin.
+ */
+package com.cthing.cmakeed.core.utils;
