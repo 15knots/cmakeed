3dac3c05212349fde5274690a956faca26f6a8b6
M. Jackson <imikejackson@users.sf.net>
2008-12-29 20:39:47 +0000

    Updating to 1.1.0 Release Status

---
+++ 1_1_0/com.cthing.cmakeed.core/src/com/cthing/cmakeed/core/package-info.java
@@ -0,0 +1,9 @@
+/* *****************************************************************************
+ * Copyright 2007 C Thing Software
+ * All Rights Reserved.
+ ******************************************************************************/
+
+/**
+ * Top level classes for the CMakeEd core plugin.
+ */
+package com.cthing.cmakeed.core;
---
+++ 1_1_0/com.cthing.cmakeed.ui.tests/src/com/cthing/cmakeed/ui/editor/CMakeEditorConfigurationTest.java
@@ -0,0 +1,98 @@
+/* *****************************************************************************
+ * Copyright 2007 C Thing Software
+ * All Rights Reserved.
+ ******************************************************************************/
+
+package com.cthing.cmakeed.ui.editor;
+
+import org.eclipse.jface.text.ITextHover;
+import org.eclipse.jface.text.contentassist.IContentAssistant;
+import org.eclipse.jface.text.presentation.IPresentationReconciler;
+
+import com.cthing.cmakeed.ui.EditorTestCase;
+
+/**
+ * Tests the CMakeEditorCOnfiguration class.
+ */
+public class CMakeEditorConfigurationTest extends EditorTestCase
+{
+    private CMakeEditorConfiguration conf;
+
+    /**
+     * Default constructor for the class.
+     */
+    public CMakeEditorConfigurationTest()
+    {
+    }
+    
+    /**
+     * {@inheritDoc}
+     * @see com.cthing.cmakeed.ui.EditorTestCase#setUp()
+     */
+    @Override
+    protected void setUp() throws Exception
+    {
+        super.setUp();
+
+        this.conf = new CMakeEditorConfiguration(new ColorMgr());
+    }
+
+    /**
+     * Tests the getConfiguredContentTypes method.
+     */
+    public void testGetConfiguredContentTypes()
+    {
+        final String[] contentTypes =
+            this.conf.getConfiguredContentTypes(getViewer());
+        assertNotNull(contentTypes);
+        assertEquals(CMakePartitionScanner.CONTENT_TYPES, contentTypes);
+    }
+    
+    /**
+     * Tests the getIndentPrefixes method.
+     */
+    public void testGetIndentPrefixes()
+    {
+        final String[] prefixes = this.conf.getIndentPrefixes(getViewer(),
+                CMakePartitionScanner.COMMAND_CONTENT_TYPE);
+        assertNotNull(prefixes);
+        assertTrue(prefixes.length > 0);
+    }
+    
+    /**
+     * Tests the getContentAssist method.
+     */
+    public void testGetContentAssist()
+    {
+        final IContentAssistant ca = this.conf.getContentAssistant(getViewer());
+        assertNotNull(ca);
+    }
+    
+    /**
+     * Tests the getTextHover method.
+     */
+    public void testGetTextHover()
+    {
+        ITextHover hover = this.conf.getTextHover(getViewer(),
+                CMakePartitionScanner.COMMAND_CONTENT_TYPE);
+        assertNotNull(hover);
+        
+        hover = this.conf.getTextHover(getViewer(),
+                CMakePartitionScanner.DEP_COMMAND_CONTENT_TYPE);
+        assertNotNull(hover);
+        
+        hover = this.conf.getTextHover(getViewer(),
+                CMakePartitionScanner.COMMENT_CONTENT_TYPE);
+        assertNull(hover);
+    }
+    
+    /**
+     * Tests the getPresentationReconciler method.
+     */
+    public void testGetPresentationReconciler()
+    {
+        final IPresentationReconciler pr =
+            this.conf.getPresentationReconciler(getViewer());
+        assertNotNull(pr);
+    }
+}
