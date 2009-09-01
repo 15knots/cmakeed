0fdb9751a096209cfc87ffdc3d5dd74a56c254ba
M. Jackson <imikejackson@users.sf.net>
2009-09-01 14:03:30 +0000

    Didn't get all the proper files

--- 1_1_4/com.cthing.cmakeed.ui/src/com/cthing/cmakeed/ui/editor/package-info.java
+++
@@ -1,9 +0,0 @@
-/* *****************************************************************************
- * Copyright 2007 C Thing Software
- * All Rights Reserved.
- ******************************************************************************/
-
-/**
- * CMake editor classes.
- */
-package com.cthing.cmakeed.ui.editor;
--- 1_1_4/com.cthing.cmakeed.ui/src/com/cthing/cmakeed/ui/editor/rules/package-info.java
+++
@@ -1,9 +0,0 @@
-/* *****************************************************************************
- * Copyright 2007 C Thing Software
- * All Rights Reserved.
- ******************************************************************************/
-
-/**
- * Rules for the CMake editor scanners.
- */
-package com.cthing.cmakeed.ui.editor.rules;
