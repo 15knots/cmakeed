9abdba1ec5f985831a937dae5cbb048a3eb03e32
M. Jackson <imikejackson@users.sf.net>
2010-02-01 18:39:08 +0000

    Bad tag specification

--- https:/cmakeed.svn.sourceforge.net/svnroot/cmakeed/tags/1_1_5/com.cthing.cmakeed.ui/src/com/cthing/cmakeed/ui/package-info.java
+++
@@ -1,9 +0,0 @@
-/* *****************************************************************************
- * Copyright 2007 C Thing Software
- * All Rights Reserved.
- ******************************************************************************/
-
-/**
- * Top level classes for the CMakeEd UI plugin.
- */
-package com.cthing.cmakeed.ui;
