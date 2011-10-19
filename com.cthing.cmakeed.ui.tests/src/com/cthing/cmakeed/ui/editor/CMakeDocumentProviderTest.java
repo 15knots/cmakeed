21499774181e220e4197f61ce8188f0116ef3731
M. Jackson <imikejackson@users.sf.net>
2011-10-19 19:04:30 +0000

    Updating to coincide with CMake 2.8.6 release

--- com.cthing.cmakeed.ui.tests/src/com/cthing/cmakeed/ui/editor/CMakeDocumentProviderTest.java
+++ com.cthing.cmakeed.ui.tests/src/com/cthing/cmakeed/ui/editor/CMakeDocumentProviderTest.java
@@ -3,12 +3,11 @@
  * All Rights Reserved.
  ******************************************************************************/
 
-package com.cthing.cmakeed.ui.editor;
+/*
+ * This class needs to be updated for the CMakeDocumentSetupParticipant class
+ */
 
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.jface.text.IDocument;
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.part.FileEditorInput;
+package com.cthing.cmakeed.ui.editor;
 
 import com.cthing.cmakeed.ui.EditorTestCase;
 
@@ -17,62 +16,5 @@ import com.cthing.cmakeed.ui.EditorTestCase;
  */
 public class CMakeDocumentProviderTest extends EditorTestCase
 {
-    /**
-     * Test wrapper class.
-     */
-    private class DocumentProvider extends CMakeDocumentProvider
-    {
-        /**
-         * Default constructor for the class.
-         */
-        public DocumentProvider()
-        {
-        }
-        
-        /**
-         * {@inheritDoc}
-         * @see com.cthing.cmakeed.ui.editor.CMakeDocumentProvider#createDocument(java.lang.Object)
-         */
-        @Override
-        protected IDocument createDocument(final Object element) throws CoreException
-        {
-            return super.createDocument(element);
-        }
-        
-        /**
-         * Allows the test class to create a document using the provider.
-         * 
-         * @param input  Editor input
-         * @return Created document
-         * @throws CoreException If there is a problem creating the document.
-         */
-        public IDocument makeDocument(final IEditorInput input) throws CoreException
-        {
-            return createDocument(input);
-        }
-    }
-
-    /**
-     * Default constructor for the class.
-     */
-    public CMakeDocumentProviderTest()
-    {
-    }
-    
-    /**
-     * Tests the createDocument method.
-     */
-    public void testCreateDocument()
-    {
-        final FileEditorInput input = new FileEditorInput(getFile());
-        final DocumentProvider provider = new DocumentProvider();
-        
-        try {
-            final IDocument doc = provider.makeDocument(input);
-            assertNotNull(doc);
-        }
-        catch (final CoreException e) {
-            fail(e.getMessage());
-        }
-    }
+   
 }
