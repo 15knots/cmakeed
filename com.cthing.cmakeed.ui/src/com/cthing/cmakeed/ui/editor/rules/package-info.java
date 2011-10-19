c8efa1cd47bf6edee63623cc4d660c84cdc26818
M. Jackson <imikejackson@users.sf.net>
2011-10-19 19:30:25 +0000

    Tagging this release

---
+++ com.cthing.cmakeed.ui/src/com/cthing/cmakeed/ui/editor/rules/package-info.java
@@ -0,0 +1,9 @@
+/* *****************************************************************************
+ * Copyright 2007 C Thing Software
+ * All Rights Reserved.
+ ******************************************************************************/
+
+/**
+ * Rules for the CMake editor scanners.
+ */
+package com.cthing.cmakeed.ui.editor.rules;
