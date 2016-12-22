45c03564c897c50e16132b7a4fb06adacc02fdf9
15knots <15knots@users.noreply.github.com>
2016-12-22 20:58:21 +0000

    Allow to specify additional commands in separate plugins

--- com.cthing.cmakeed.core/src/com/cthing/cmakeed/core/commands/CMakeCommands.java
+++ com.cthing.cmakeed.core/src/com/cthing/cmakeed/core/commands/CMakeCommands.java
@@ -5,8 +5,12 @@
 
 package com.cthing.cmakeed.core.commands;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.LinkedHashMap;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.eclipse.core.runtime.IConfigurationElement;
@@ -23,44 +27,49 @@ import com.cthing.cmakeed.core.utils.StringUtils;
 public final class CMakeCommands
 {
     private static final String COMMANDS_EXT_ID = "commands";  //$NON-NLS-1$
-    
+
     private static final String ATT_NAME = "name";  //$NON-NLS-1$
     private static final String ATT_DESC = "desc";  //$NON-NLS-1$
     private static final String ATT_DEPRECATED = "deprecated";  //$NON-NLS-1$
     private static final String ATT_VALUE = "value";  //$NON-NLS-1$
-        
+
     private static Map<String, CMakeCommand> commands;
-    
+
+    private static List<CMakeCommand> commandsSorted ;
+
     /**
      * Not to be instantiated.
      */
     private CMakeCommands()
     {
     }
-    
+
     /**
      * Loads the command information from the plugin extension point.
      */
     private static void loadCommands()
     {
-        commands = new LinkedHashMap<String, CMakeCommand>();
+        commands = new HashMap<String, CMakeCommand>();
         
         final IExtensionPoint extensionPoint = Platform.getExtensionRegistry().getExtensionPoint(CorePlugin.PLUGIN_ID, COMMANDS_EXT_ID);
         final IExtension[] extensions = extensionPoint.getExtensions();
-        
+
         for (IExtension extension : extensions) {
             if (extension.isValid()) {
                 final IConfigurationElement[] commandElts = extension.getConfigurationElements();
-                
+
                 for (IConfigurationElement commandElt : commandElts) {
                     final String name = commandElt.getAttribute(ATT_NAME);
                     final String desc = commandElt.getAttribute(ATT_DESC);
                     final Boolean deprecated = Boolean.valueOf(commandElt.getAttribute(ATT_DEPRECATED));
 
-                    final CMakeCommand cmd = new CMakeCommand(name, desc, deprecated);
-                    commands.put(name, cmd);
+                    CMakeCommand cmd= commands.get(name);
+                    if(cmd==null){
+                        cmd = new CMakeCommand(name, desc, deprecated);
+                        commands.put(name, cmd);
+                    }
                     //System.out.println(cmd.toString());
-                    
+
                     final IConfigurationElement[] usageElts = commandElt.getChildren();
                     for (IConfigurationElement usageElt : usageElts) {
                         cmd.addUsage(usageElt.getAttribute(ATT_VALUE));
@@ -69,10 +78,10 @@ public final class CMakeCommands
             }
         }
     }
-    
+
     /**
      * Obtains information for the specified command.
-     * 
+     *
      * @param name  Name of the command (case insensitive).
      * @return Command information object.
      */
@@ -81,13 +90,14 @@ public final class CMakeCommands
         if (commands == null) {
             loadCommands();
         }
-        
+
         return (StringUtils.isBlank(name)) ? null : commands.get(name.toLowerCase());
     }
-    
+
     /**
-     * Provides all commands.
-     * 
+     * Provides all commands. The returned collection is guaranteed to be sorted first by command
+     * name and secondly by usage description text.
+     *
      * @return A collection containing all commands.
      */
     public static Collection<CMakeCommand> getCommands()
@@ -95,7 +105,18 @@ public final class CMakeCommands
         if (commands == null) {
             loadCommands();
         }
-        
-        return commands.values();
+        if (commandsSorted == null) {
+            // sort commands by name and usage text..
+            commandsSorted = new ArrayList<CMakeCommand>(commands.values());
+            Collections.sort(commandsSorted, new Comparator<CMakeCommand>() {
+
+              public int compare(CMakeCommand c1, CMakeCommand c2) {
+                final int byName = c1.getName().compareToIgnoreCase(c2.getName());
+                return byName!=0 ? byName: c1.getUsages()[0].compareToIgnoreCase(c2.getUsages()[0]);
+              }
+            });
+        }
+
+        return commandsSorted;
     }
 }
